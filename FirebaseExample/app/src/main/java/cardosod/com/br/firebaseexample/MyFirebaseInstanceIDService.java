24dc22f1d812dd144896949750a3afe714a9b8d9
Diego Cardoso Alves <diego.alves@eldorado.org.br>
2016-07-27 09:49:07 -0300

    Sending Firebase Example

---
+++ FirebaseExample/app/src/main/java/cardosod/com/br/firebaseexample/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package cardosod.com.br.firebaseexample;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by cardosod on 26/07/2016.
+ */
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
