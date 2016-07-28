bda3b8e03031b508f5fbb2a2f38721e6f9c4ac3a
fabioej <fabio.inatel@gmail.com>
2016-07-28 14:24:48 +0100

    Changed main Layout

--- FirebaseExample/app/src/main/java/cardosod/com/br/firebaseexample/MainActivity.java
+++ FirebaseExample/app/src/main/java/cardosod/com/br/firebaseexample/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.os.Build;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.view.*;
+import android.widget.RadioGroup.*;
+
 
 import com.firebase.client.Firebase;
 import com.google.firebase.database.*;
@@ -19,14 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //dynamic menu
+        final RadioGroup group = (RadioGroup) findViewById(R.id.rgTerms);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group,int checkedId){
+                RadioButton button = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+
         MyFirebaseInstanceIDService service = new MyFirebaseInstanceIDService();
         //service.sendRegistrationToServer();
 
         Firebase ref = new Firebase("https://meetyournextmoto-b9daf.firebaseio.com/");
 
-        Log.d("Pramesh: ", service.getToken());
-        ref.child("Serial number").child(Build.SERIAL).setValue(service.getToken());
+        //Log.d("Pramesh: ", service.getToken());
+        ref.child("Serial number").child(Build.SERIAL).setValue("test");
     }
+
+    //OnCheckedChanged onCheckedChanged = new OnCheckedChanged() {
+    OnCheckedChangeListener onCheckedChanged = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.rbAgree:
+                    TextView TextName = (TextView) findViewById(R.id.tvName);
+                    TextName.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
 
 
